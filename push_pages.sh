5e88345fac297a0f4f416d6628285455905cca8d
Jeeva Kandasamy <jkandasa@gmail.com>
2016-03-01 19:52:36 +0530

    MyController.org page update

---
+++ push_pages.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git checkout pages && mvn install -B && git checkout master && cp target/website/* . -r && rm target/ -rf && git add * && git commit -m "MyController.org page update" && git push orign master;
+
+# back to pages branch
+git checkout pages;
