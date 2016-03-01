51bf0e9aea5a3c117b0f2c743f79e1a9ccc7cb57
Jeeva Kandasamy <jkandasa@gmail.com>
2016-03-01 19:58:44 +0530

    removed script

--- push_pages.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git checkout pages && mvn install -B && git checkout master && cp target/website/* . -r && rm target/ -rf && git add * && git commit -m "MyController.org page update" && git push orign master;
-
-# back to pages branch
-git checkout pages;
